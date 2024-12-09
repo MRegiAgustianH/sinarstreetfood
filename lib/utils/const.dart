c5637b665ad62c574b34d270248b92da7aae00c4
MRegiAgustianH <muhmmad.regi@gmail.com>
2024-12-10 00:37:28 +0700

    Update

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kButtonColor = Color(0xFF1B1B1B);
 const kBackgroundColor = Color(0xFFF0F3F5);
 const blueTextColor = Color(0xFF2B56CC);
+const BackgroundHome = Color(0xFF007AFF);
